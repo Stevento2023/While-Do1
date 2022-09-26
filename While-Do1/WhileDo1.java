c11d159320939f87f53943ef1b2f4c74d4ccec2d
Steven To <steven.to@mrjaffesclass.com>
2022-09-26 11:36:13 -0700

    Forloops work

--- While-Do1/WhileDo1.java
+++ While-Do1/WhileDo1.java
@@ -7,12 +7,16 @@ public class WhileDo1
     int d = -8;
     
     public void run() {
-        int i = 0;
-        while (i <10) {
-            System.out.println("i is " + i + " but is less than 10");
-            i += 1;
+        for (int i = 0; i <= 15; i = i+1) {
+            System.out.println("i is equal to "+i);
         }
-    }
+        for (int j = 10; j >= 0; j = j-2) {
+            System.out.println("j is equal to "+j);
+        }
+        for (int k = 10; k >= 0; k = k-3) {
+            System.out.println("k is equal to "+k);
+        }
+        }
     
     public static void main(String[] args)
         {
